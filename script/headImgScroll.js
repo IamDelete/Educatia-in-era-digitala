53418214df7bc2dddc3fc582c9ba43593b42f07c
Teo <th3od0ry@gmail.com>
2024-01-19 18:18:29 +0200

    scroll animation

---
+++ script/headImgScroll.js
@@ -0,0 +1,15 @@
+const image = document.getElementById('headImg');
+const title = document.getElementById('titleContainer');
+const subTitle = document.getElementById('welcome');
+
+title.style.transition = 'all .4s ease-in-out';
+subTitle.style.transition = title.style.transition;
+
+window.addEventListener('scroll', () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const maxScroll = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollPercentage = scrollTop / maxScroll;
+  const translateY = scrollPercentage * 100;
+  image.style.transform = `translateY(${translateY}%)`;
+
+});
